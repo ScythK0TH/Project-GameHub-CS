5a011c24f731b7e5e0f732a1426f908b1a8a315b
ScythK0TH <150907808+ScythK0TH@users.noreply.github.com>
2023-11-15 20:16:49 +0700

    Add files via upload

--- gamehub.py
+++ gamehub.py
@@ -24,11 +24,10 @@ menu = ConsoleMenu("Main Menu All-in-one Games By CS", '''
 
 # Create some items
 
-worda = ConsoleMenu("Select Version", ''' _       __               __   ___       __                 __                
-| |     / /___  _________/ /  /   | ____/ /   _____  ____  / /___  __________ 
-| | /| / / __ \/ ___/ __  /  / /| |/ __  / | / / _ \/ __ \/ __/ / / / ___/ _ \
-| |/ |/ / /_/ / /  / /_/ /  / ___ / /_/ /| |/ /  __/ / / / /_/ /_/ / /  /  __/
-|__/|__/\____/_/   \__,_/  /_/  |_\__,_/ |___/\___/_/ /_/\__/\__,_/_/   \___/''', formatter=MenuFormatBuilder())
+worda = ConsoleMenu("Select Version", '''  _      __            __  _____              
+ | | /| / /__  _______/ / / ___/__ ___ _  ___ 
+ | |/ |/ / _ \/ __/ _  / / (_ / _ `/  ' \/ -_)
+ |__/|__/\___/_/  \_,_/  \___/\_,_/_/_/_/\__/''', formatter=MenuFormatBuilder())
 wordas = SubmenuItem("Word Adventure", worda, menu)
 command_item1 = CommandItem("Word Adventure",  "main.py")
 word_hard = FunctionItem("Word Adventure (Main Game)", wordmainextra)
@@ -49,12 +48,10 @@ selection_menu.append_item(wordas)
 # A SubmenuItem lets you add a menu (the selection_menu above, for example)
 # as a submenu of another menu
 submenu_item = SubmenuItem("Play", selection_menu, menu)
-submenu_2 = ConsoleMenu("Third Submenu", "This Time with Double-Line Borders.",
-                            prologue_text="This is my prologue. I am currently showing my top and bottom borders, but \
-they are hidden by default. Also notice that my text is really long, so it extends beyond a single line, and should \
-wrap properly within the menu borders. This is a useful place to put instructions to the user about how to use \
-the menu.",
-                            epilogue_text="This is my epilogue. My borders are currently hidden.",
+submenu_2 = ConsoleMenu("Credits", "This is prototype version of CS Game Hub Project made by me.",
+                            prologue_text="“This is a Python games project that brings together everyone’s games in the team.\
+Thank you for playing this game. We would like to thank our teacher for teaching us. We hope you enjoy these games!”",
+                            epilogue_text="This is my first project. If you find any issues contract us.",
                             formatter=MenuFormatBuilder()
                             .set_title_align('center')
                             .set_subtitle_align('center')
